5e143097f25895db73cb37d8849616b2c78db222
Kayt1020 <143145652+Kayt1020@users.noreply.github.com>
2023-10-24 05:55:30 +0000

    progress

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-function randomPokerCard() {
-    const suits=[ '♦', '♥', '♠', '♣'];
-    const values= ['2', '3', '4', '5', '6', '7', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
+import '../style/styles.css';
 
-    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
-    const randomValue = values[Math.floor(Math.random() * values.length)];
+    //This will include file.js into your bundle.
+    import 'js/file2.js';
+    
+    //this will include the styles at index.scss to your bundle.
+    import '../styles/index.scss';
 
-    document.querySelector(".topSuit").innerHTML = randomSuit
-    document.querySelector(".botSuit").innerHTML = randomSuit
-    document.querySelector(".cardSuit".innerHTML = randomSuit
 
-    if (randomSuit) *** "heart icon" || randomSuit *** 
-}
+window.onload = function(){
+    console.log('Random Card Gen');
+
+    document.querySelector('card').classList.add('genRandS');
+    document.querySelector('card').innerHTML = ('genRandNum');
+ 
+};
+
+    let genRandNum = () =>{
+        let numbers =["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+        let indexNumbers = Math.floor(Math.random()) * numbers.length);
+        return numbers[indexNumbers];
+    };
+
+    let genRandS = () => {        
+    let suit =["diamond", "spades", "hearts", "clubs",];
+    let indexSuit = Math.floor(Math.random()* suit.length);
+
+    return suit[indexSuit];
+    };
+
+        let numb = Math.floor(Math.random()* 12);
+    };
+        
+        return numbers[numb];
+};    
+    let genRandSuit = () =>{    
+        let suit = ["&spades;", "&clubs;","&hearts;","&diams;"];
+        let indexSuit = Math.floor(Math.random()*suit.length);
+        return suit[indexSuit];
+};
