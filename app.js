faaddbce4fb0aab0bc7c765a5bf063a60edf68af
Kayt1020 <143145652+Kayt1020@users.noreply.github.com>
2023-10-28 19:13:00 +0000

    chnages

--- app.js
+++ app.js
@@ -3,38 +3,41 @@ import '../style/styles.css';
     //This will include file.js into your bundle.
     import 'js/file2.js';
     
-    //this will include the styles at index.scss to your bundle.
-    import '../styles/index.scss';
+    //this will include the styles at index.css to your bundle.
+    import '../styles/index.css';
 
 
 window.onload = function(){
-    console.log('Random Card Gen');
 
-    document.querySelector('card').classList.add('genRandS');
-    document.querySelector('card').innerHTML = ('genRandNum');
- 
-};
-
-    let genRandNum = () =>{
-        let numbers =["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-        let indexNumbers = Math.floor(Math.random()) * numbers.length);
-        return numbers[indexNumbers];
+    window.genCard= function(){
+    
+        var suit = ["♥","♠️","♣️","♦"];
+        var numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    
+            function randomCard(){
+    
+                var randomSuit = suit[Math.floor(Math.random()*suit.length)];
+                var randomNumber = numbers[Math.floor(Math.random()*numbers.length)];
+    
+                document.querySelector(".icon").innerHTML = randomSuit;
+                document.querySelector(".cardContentContainer").innerHTML = randomNumber;
+                document.querySelector(".iconTwo").innerHTML = randomSuit;
+    
+                if (randomSuit=="♥" || randomSuit=="♦"){
+                    document.querySelector("#maincardcontainer").style.color = "red";
+                }
+    
+                else if (randomSuit=="♠" || randomSuit=="♣️"){
+                    document.querySelector("#maincardcontainer").style.color = "black";
+    
+                }
+    
+            }
+    
+        randomCard();
+    
+    
+        };
+    
+        window.genCard();
     };
-
-    let genRandS = () => {        
-    let suit =["diamond", "spades", "hearts", "clubs",];
-    let indexSuit = Math.floor(Math.random()* suit.length);
-
-    return suit[indexSuit];
-    };
-
-        let numb = Math.floor(Math.random()* 12);
-    };
-        
-        return numbers[numb];
-};    
-    let genRandSuit = () =>{    
-        let suit = ["&spades;", "&clubs;","&hearts;","&diams;"];
-        let indexSuit = Math.floor(Math.random()*suit.length);
-        return suit[indexSuit];
-};
