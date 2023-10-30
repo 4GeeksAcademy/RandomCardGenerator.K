3b7d1d1c8189909966db24119087101565e993d3
Kayt1020 <143145652+Kayt1020@users.noreply.github.com>
2023-10-30 19:00:45 +0000

    10.29

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ window.onload = function(){
 
     // arrays with the numbers and suits for each card   
 let cardType = ["♥","♠️","♣️","♦"];
-let cardNumber = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+let cardNumb = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
 function randomElement(mainCard){
     return mainCard[Math.floor(Math.random() * mainCard.length)];
 }
-let center = randomElement (cardNumber);
+let cardNumber = randomElement (cardNumb);
 let topSuit = randomElement(cardType);
 let bottomSuit = randomElement(cardType);
 
@@ -19,7 +19,7 @@ const p2Element = document.getElementsByClassName("cardNumber");
 const p3Element = document.getElementsByClassName("bottomSuit");
 
 p1Element[0].innerText = topSuit;
-p2Element[0].innerText = center;
+p2Element[0].innerText = cardNumber;
 p3Element[0].innerText = bottomSuit;
 
 function colorSuits (suit){
@@ -31,7 +31,7 @@ if (suit === "♦" || suit === "♥" ){
 }
 
 p1Element[0].style.color = colorSuits (topSuit);
-p2Element[0].style.color = colorSuits (center);
+p2Element[0].style.color = colorSuits (cardNumber);
 p3Element[0].style.color= colorSuits (bottomSuit);
 };
         
